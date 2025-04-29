2af9cbeff48a3b0366f0eb0c42b2f11e8a2e0fc4
Junajoy <junamangalath@gmail.com>
2025-04-29 09:32:48 +0530

    git

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/QuizPage.jsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Question from '../components/questions/Question';
@@ -8,13 +7,37 @@ const QuizPage = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [questions, setQuestions] = useState([]);
   const [score, setScore] = useState(0);
+  const [answers, setAnswers] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     setQuestions(questionData);
   }, []);
 
-  const recordAnswer = (isCorrect) => {
+  const recordAnswer = (question, selectedOptions) => {
+    const correctAnswers = question.options
+      .filter((o) => o.isCorrect)
+      .map((o) => o.text)
+      .sort();
+
+    const selected = [...selectedOptions].sort();
+
+    const isCorrect =
+      correctAnswers.length === selected.length &&
+      correctAnswers.every((val, index) => val === selected[index]);
+
+      setAnswers((prev) => [
+        ...prev,
+        {
+          question: question.question,
+          code: question.code || null,
+          correctAnswers,
+          selectedAnswers: selected,
+          isCorrect,
+        },
+      ]);
+      
+
     if (isCorrect) {
       setScore((prevScore) => prevScore + 1);
     }
@@ -28,10 +51,9 @@ const QuizPage = () => {
       navigate('/quiz-end', {
         state: {
           score: percentage,
-          questions: questions
-        }
+          answers: answers,
+        },
       });
-      
     }
   };
 
